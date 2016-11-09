e057a8c1f703e54c5387bcb28a1962b3f93bc721
qhuydtvt <qhuydtvt@gmail.com>
2016-11-09 20:23:06 +0700

    Changed to songs API

--- app.py
+++ app.py
@@ -3,38 +3,45 @@ from mlab import *
 from mongoengine import *
 
 app = Flask(__name__)
-mlab_connect()
 
-class Post(Document):
-    title = StringField()
-    content = StringField()
-    def to_json(self):
-        return {"title": self.title, "content":self.content}
+songs = [
+    {
+      "title": "Hello Hello",
+      "artist": "FT Island",
+      "genre": "Hàn Quốc, Pop / Ballad",
+      "source": "http://api.mp3.zing.vn/api/mobile/source/song/LGJGTLGNVEDLVAGTLDJTDGLG"
+    },
+    {
+      "title": "Faded",
+      "artist": "Alan Walker",
+      "genre": "Âu Mỹ, Electronic / Dance",
+      "source": "http://api.mp3.zing.vn/api/mobile/source/song/LGJGTLGNQAQJNLVTLDJTDGLG"
+    },
+    {
+      "title": "Heathens",
+      "artist": "Twenty One Pilots",
+      "genre": "Âu Mỹ, Pop, Nhạc Phim",
+      "source": "http://api.mp3.zing.vn/api/mobile/source/song/LGJGTLGNQQVNLNJTLDJTDGLG"
+    },
+    {
+      "title": "Clap Hands",
+      "artist": "Tom Waits",
+      "genre": "Âu Mỹ, Rock",
+      "source": "http://api.mp3.zing.vn/api/mobile/source/song/LGJGTLGNVNXQEQJTLDJTDGLG"
+    },
+    {
+      "title": "Everybody Knows",
+      "artist": "Ride",
+      "genre": "Âu Mỹ, Rock",
+      "source": "http://api.mp3.zing.vn/api/mobile/source/song/LGJGTLGNAAQEEGDTLDJTDGLG"
+    }
+]
 
-@app.route('/', methods=["GET", "POST"])
-def hello_world():
-    if request.method == "GET":
-        return jsonify([post.to_json() for post in Post.objects])
-    elif request.method == "POST":
-        form = request.get_json()
-        title_value = form["title"]
-        content_value = form["content"]
 
-        p = Post(title=title_value, content=content_value)
-        p.save()
-
-        return jsonify({"code": 1, "message": "OK"})
-
-@app.route('/api/login', methods=["POST"])
-def login():
-    if request.method == "POST":
-        json_data = request.get_json()
-        username = json_data["username"]
-        password = json_data["password"]
-        if username == "techkids" and password == "codethechange":
-            return jsonify({"code": 1, "message": "Logged in"})
-        else:
-            return jsonify({"code": 0, "message": "Failed to login"})
+@app.route("/api/songs")
+@app.route("/api/songs/")
+def api_songs():
+    return jsonify(songs)
 
 if __name__ == '__main__':
     app.run()
